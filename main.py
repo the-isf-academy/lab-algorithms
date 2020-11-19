bb063821666d03750ab035cc9a70818cf8deeb52
Jacob Wolf <jacob.h.wolf@gmail.com>
2020-11-19 13:37:58 +0800

    Live coded 3 approaches to DFS and BFS in class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from graph import Graph, Vertex
-from search import dfs, bfs
+from search import dfs, bfs, dfs0a, dfs0b, dfs1
 
 mtr = {
         "syp": ["syp", "sheung wan"],
@@ -34,26 +34,26 @@ loop_matrix = [
 print("Basic searches:")
 graph = Graph(graph_dict=simple_graph)
 print(graph)
-print(' -> '.join(map(str, bfs("a", "c", graph))))
+print(' -> '.join(map(str, dfs1("a", "c", graph))))
 graph.reset()
-print(' -> '.join(map(str, bfs("a", "a", graph))))  # Same start/end
+print(' -> '.join(map(str, dfs1("a", "a", graph))))  # Same start/end
 graph.reset()
-print(' -> '.join(map(str, bfs("a", "d", graph))))  # No path exists
-
-# Search path from SYP to Jordan in MTR graph from slides
+print(' -> '.join(map(str, dfs1("a", "d", graph))))  # No path exists
+# #
+# # # Search path from SYP to Jordan in MTR graph from slides
 print("\nMTR graph searches:")
 graph = Graph(graph_dict=mtr)
 print(graph)
-print(' -> '.join(map(str, dfs("syp", "jordan", graph))))
-
-# Loop graph from C.2
+print(' -> '.join(map(str, bfs("syp", "jordan", graph))))
+#
+# # Loop graph from C.2
 print("\nLoop graph searches:")
 graph = Graph(graph_matrix=loop_matrix)
 print(graph)
-print(' -> '.join(map(str, dfs("a", "h", graph))))
+print(' -> '.join(map(str, bfs("h", "a", graph))))
 graph.reset()
-print(' -> '.join(map(str, bfs("a", "h", graph))))
-
-
-# ADD YOUR OWN SEARCHES HERE
-print("\nAdditional searches:")
+print(' -> '.join(map(str, dfs0b("h", "a", graph))))
+#
+#
+# # ADD YOUR OWN SEARCHES HERE
+# print("\nAdditional searches:")
